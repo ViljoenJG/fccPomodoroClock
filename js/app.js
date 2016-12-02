78801d6eada480aa1a380eda127d1618be56952c
Kobus Viljoen <kobus.viljoen@trivector.co.za>
2016-12-02 15:34:21 +0200

    Added rest timer logic

--- js/app.js
+++ js/app.js
@@ -1,40 +1,50 @@
-const defaultMinutes = 25;
-const defaultSeconds = 0;
+const defaultTimer = 25;
+const defaultRest = 5;
 
-let minutes = defaultMinutes;
-let startTime = null;
+let timerMinutes = defaultTimer;
+let restMinutes = defaultRest;
 let timer = null;
 
 function startTimer() {
-    startTime = Date.now();
-    timer = setTimeout(updateTimer, 100)
+    const timerStartTime = Date.now();
+    timer = setTimeout(updateTimer, 100, timerStartTime, timerMinutes, true);
 }
 
-function updateTimer() {
+function startRest() {
+    const restStartTime = Date.now();
+    timer = setTimeout(updateTimer, 100, restStartTime, restMinutes, false);
+}
+
+function updateTimer(startTime, interval, isTimer) {
     let diff = Date.now() - startTime;
     let minutesDiff = Math.ceil(diff / 60000);
     let secondsDiff = Math.ceil((diff % 60000) / 1000);
     
-    let minuteVal =  minutes - minutesDiff;
+    let minuteVal =  interval - minutesDiff;
     let secondsVal = (60 - secondsDiff < 10) 
         ? '0' + (60 - secondsDiff) : 60 - secondsDiff;
+
+    minuteVal = (minuteVal <= 0) ? minuteVal = '00' : minuteVal;
+    secondsVal = (secondsVal <= 0) ? secondsVal = '00' : secondsVal;
     
-    if (minuteVal <= 0) {
-        minuteVal = '00';
+    document.getElementById('minutes').innerText = '' + minuteVal;
+    document.getElementById('seconds').innerText = '' + secondsVal;
+
+    if (minuteVal == '00' && secondsVal == '00') {
+        if (isTimer) {
+            startRest();
+        } else {
+            startTimer();
+        }
+
+        return;
     }
     
-    if (secondsVal <= 0) {
-        secondsVal = '00';
-    }
-    
-    document.getElementById('minutes').innerText = minuteVal;
-    document.getElementById('seconds').innerText = secondsVal;
-    
-    timer = setTimeout(updateTimer, 100);
+    timer = setTimeout(updateTimer, 100, startTime, interval, isTimer);
 }
 
 function resetClock() {
     window.clearTimeout(timer);
-    document.getElementById('minutes').innerText = minutes;    
+    document.getElementById('minutes').innerText = '' + timerMinutes;
     document.getElementById('seconds').innerText = '00';
 }
